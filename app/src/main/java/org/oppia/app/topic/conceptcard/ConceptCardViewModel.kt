dd59726dd7a07a4c2f020c54a5c72f656dfb15e6
James Xu <jamesxu@umich.edu>
2019-09-25 16:58:10 -0700

    Added transformation to livedata

--- app/src/main/java/org/oppia/app/topic/conceptcard/ConceptCardViewModel.kt
+++ app/src/main/java/org/oppia/app/topic/conceptcard/ConceptCardViewModel.kt
@@ -1,22 +1,33 @@
 package org.oppia.app.topic.conceptcard
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.processNextEventInCurrentThread
 import org.oppia.app.fragment.FragmentScope
 import org.oppia.app.model.ConceptCard
 import org.oppia.domain.topic.TEST_SKILL_ID_0
 import org.oppia.domain.topic.TopicController
 import org.oppia.util.data.AsyncResult
+import org.oppia.util.logging.Logger
 import javax.inject.Inject
 
 /** [ViewModel] for concept card, providing rich text and worked examples */
 @FragmentScope
 class ConceptCardViewModel @Inject constructor(
-  private val topicController: TopicController
-  ) : ViewModel() {
+  private val topicController: TopicController,
+  private val logger: Logger
+) : ViewModel() {
   private val skillId: String = TEST_SKILL_ID_0
 
-  fun getConceptCardLiveData(): LiveData<AsyncResult<ConceptCard>> {
-    return topicController.getConceptCard(skillId)
+  fun getConceptCardLiveData(): LiveData<ConceptCard> {
+    return Transformations.map(topicController.getConceptCard(skillId), ::processConceptCardResult)
+  }
+
+  private fun processConceptCardResult(conceptCardResult: AsyncResult<ConceptCard>): ConceptCard {
+    if (conceptCardResult.isFailure()) {
+      logger.e("ConceptCardFragment", "Failed to retrieve Concept Card: " + conceptCardResult.getErrorOrNull())
+    }
+    return conceptCardResult.getOrDefault(ConceptCard.getDefaultInstance())
   }
 }
