276d21005f81060b170fb1cf0dd49539db233b00
James Xu <jamesxu@umich.edu>
2019-10-22 15:46:50 -0700

    Fixed explorationProgressController comments

--- app/src/main/java/org/oppia/app/topic/questionplayer/QuestionPlayerViewModel.kt
+++ app/src/main/java/org/oppia/app/topic/questionplayer/QuestionPlayerViewModel.kt
@@ -3,11 +3,12 @@ package org.oppia.app.topic.questionplayer
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import org.oppia.app.fragment.FragmentScope
+import org.oppia.app.viewmodel.ObservableViewModel
 import javax.inject.Inject
 
 /** [ViewModel] for concept card, providing rich text and worked examples */
 @FragmentScope
-class QuestionPlayerViewModel @Inject constructor() : ViewModel() {
+class QuestionPlayerViewModel @Inject constructor() : ObservableViewModel() {
   var isFeedbackVisible = ObservableField<Boolean>(false)
   var isSubmitButtonVisible = ObservableField<Boolean>(false)
   var isContinueButtonVisible = ObservableField<Boolean>(false)
